dbabbd7ca39fc635bedd2ee2d71877853239b878
Nameless <truemedian@gmail.com>
2021-11-11 17:38:50 -0600

    bump 0.2.0

--- package.lua
+++ package.lua
@@ -1,8 +1,8 @@
 return {
     name = 'truemedian/rethink-luvit',
-    version = '0.1.0',
+    version = '0.2.0',
     dependencies = {
-        'creationix/coro-net@3.2.1',
+        'creationix/coro-net@3.3.0',
         'luvit/secure-socket@1.2.3',
     },
     license = 'MIT',
